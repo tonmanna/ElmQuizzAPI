d4bf0e5c8e97f50fefe4d2b16f50accfc3f3c28b
Worawut WSL2 Home <worawut@theiconweb.com>
2022-11-07 03:54:34 +0700

    Add Netrify

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ const gqlRoute = require('./routers/graphql.route');
 const routeExpress = require('./routers/app.route')
 const cors = require('cors');
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 gqlRoute(app);
 routeExpress(app);
 app.listen(process.env.PORT || 4000,
   () => console.log("Server is running..."));
+
