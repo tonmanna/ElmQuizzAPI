0fd417b71cb1bdbdec9fd0dd56629d9c405753ba
Worawut WSL2 Home <worawut@theiconweb.com>
2022-11-07 22:47:35 +0700

    Update

--- src/services/mail.js
+++ src/services/mail.js
@@ -1,13 +1,13 @@
-var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
+var nodemailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
 
 exports.transporter = nodemailer.createTransport(
   smtpTransport({
-    host: 'smtp.mailchannels.net',
+    host: "smtp.mailchannels.net",
     port: 25,
     auth: {
-      user: 'itopplus',
-      pass: '80qboVVatKkYGz1R08PKsObF',
+      user: "itopplus",
+      pass: "wcNHReaU8MeTuIyS9lzlXPZd",
     },
   })
 );
